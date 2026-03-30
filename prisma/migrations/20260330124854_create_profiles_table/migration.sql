849a5a0ab89aacb2320b6b3a05f0e880a837eff8
Jayson Alpe (Wolf) <jaysonalpe@gmail.com>
2026-03-30 20:49:27 +0800

    Create Profiles table

---
+++ prisma/migrations/20260330124854_create_profiles_table/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE `profiles` (
+    `id` VARCHAR(191) NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `bio` TEXT NOT NULL,
+    `location` VARCHAR(191) NOT NULL,
+    `hometown` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
+    `phone_privacy` ENUM('public', 'friends', 'only-me') NOT NULL DEFAULT 'only-me',
+    `email_privacy` ENUM('public', 'friends', 'only-me') NOT NULL DEFAULT 'only-me',
+    `last_accessed` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `deleted_at` DATETIME(3) NULL,
+
+    UNIQUE INDEX `profiles_username_key`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `profiles` ADD CONSTRAINT `profiles_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
