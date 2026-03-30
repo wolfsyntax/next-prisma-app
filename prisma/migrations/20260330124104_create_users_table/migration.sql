d430d6ec530b5dbfb082b847a177d941f4360e6e
Jayson Alpe (Wolf) <jaysonalpe@gmail.com>
2026-03-30 20:44:49 +0800

    Add Schema: Users & Accounts

--- prisma/migrations/20260330124104_create_users_table/migration.sql
+++ prisma/migrations/20260330124104_create_users_table/migration.sql
@@ -4,13 +4,18 @@ CREATE TABLE `users` (
     `first_name` VARCHAR(191) NOT NULL,
     `last_name` VARCHAR(191) NULL,
     `email` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NOT NULL,
+    `date_of_birth` DATE NULL,
     `photo_url` VARCHAR(191) NULL,
     `password` VARCHAR(191) NULL,
     `email_verified_at` DATETIME(3) NULL,
+    `rememberToken` VARCHAR(100) NULL,
+    `last_login` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
     `deleted_at` DATETIME(3) NULL,
 
     UNIQUE INDEX `users_email_key`(`email`),
+    UNIQUE INDEX `users_phone_key`(`phone`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ prisma/migrations/20260330124229_create_accounts_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `accounts` (
+    `id` VARCHAR(191) NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+    `provider` VARCHAR(191) NOT NULL,
+    `provider_uid` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `deleted_at` DATETIME(3) NULL,
+
+    UNIQUE INDEX `accounts_provider_user_id_key`(`provider`, `user_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `accounts` ADD CONSTRAINT `accounts_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
